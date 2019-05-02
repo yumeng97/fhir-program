c5dcf2958a1e047e3f1f89d3794a2a5b31600265
ywon0008 <ywon0008@student.monash.edu>
2019-05-03 03:20:34 +1000

    Patients now showing on practioner page

--- ClientApp/components/practitioner/practitioner.ts
+++ ClientApp/components/practitioner/practitioner.ts
@@ -3,10 +3,18 @@ import axios from 'axios';
 import { Component } from 'vue-property-decorator';
 
 interface Practitioner {
-    Id: string,
+    id: string,
     name: string
     isActive: string,
-    Gender: string,
+    gender: string,
+    address: string,
+    lastUpdated: string
+}
+
+interface Patient {
+    id: string,
+    name: string
+    gender: string,
     address: string,
     lastUpdated: string
 }
@@ -14,13 +22,19 @@ interface Practitioner {
 @Component
 export default class PractitionerListComponent extends Vue {
     practitioner: Practitioner = <Practitioner>{
-        Id: "",
+        id: "",
         isActive: "",
-        Gender: ""
+        gender: "",
+        address: "",
+        lastUpdated: "",
+        name: ""
     }
+
+    patients: Patient[] = [];
 
     mounted() {
         this.getPractitioner(this.$route.params.id);
+        this.getPatients();
     }
 
     getPractitioner(id: string) {
@@ -30,7 +44,27 @@ export default class PractitionerListComponent extends Vue {
             url: url
         }).then((response: any) => {
             console.log(response.data);
-            this.practitioner = response.data;
+            this.practitioner.id = response.data.id;
+            this.practitioner.name = response.data.name.givenName + " " + response.data.name.familyName;
+            this.practitioner.isActive = response.data.active;
+            this.practitioner.gender = response.data.gender;
+            this.practitioner.lastUpdated = response.data.lastUpdated;
+            this.practitioner.address = response.data.address.line + ", " + response.data.address.city + ", " + response.data.address.state 
+                                            + ", " + response.data.address.postalCode + " " + response.data.address.country;
+
+        })
+            .catch((error: any) => {
+                console.log(error);
+            });
+    }
+
+    getPatients() {
+        axios({
+            method: 'get',
+            url: 'api/Patient/ShowAll'
+        }).then((response: any) => {
+            console.log(response.data);
+            this.patients = response.data;
         })
             .catch((error: any) => {
                 console.log(error);
