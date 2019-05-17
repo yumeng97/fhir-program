5fa533eb2b98fb1b789b0f383d59214bfe5f3148
ywon0008 <ywon0008@student.monash.edu>
2019-05-17 22:34:45 +1000

    Observable monitor reporter observation

---
+++ Observables/ObservationMonitor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using project.Models;
+
+namespace project.Observables
+{
+    public class ObservationMonitor : IObservable<Observation>
+    {
+        private List<IObserver<Observation>> observers;
+        private List<Observation> observations;
+        public string patientId { get; set; }
+
+        public ObservationMonitor()
+        {
+            observers = new List<IObserver<Observation>>();
+            observations = new List<Observation>();
+        }
+        public IDisposable Subscribe(IObserver<Observation> observer)
+        {
+            if (! observers.Contains(observer))
+            {
+                observers.Add(observer);
+                foreach (var item in observations)
+                    observer.OnNext(item);
+            }
+            return new Unsubscriber(observers, observer);
+        }
+
+        private class Unsubscriber : IDisposable
+        {
+            private List<IObserver<Observation>> _observers;
+            private IObserver<Observation> _observer;
+
+            public Unsubscriber(List<IObserver<Observation>> observers, IObserver<Observation> observer)
+            {
+                this._observers = observers;
+                this._observer = observer;
+            }
+
+            public void Dispose()
+            {
+                if (!(_observer == null)) _observers.Remove(_observer);
+            }
+        } 
+    }
+}
---
+++ Observables/ObservationReporter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using project.Models;
+
+namespace project.Observables
+{
+    public class ObservationReporter : IObserver<Observation>
+    {
+        private IDisposable unsubscriber;
+        private bool first = true;
+        private Observation last;
+
+        public virtual void Subscribe(IObservable<Observation> provider)
+        {
+            unsubscriber = provider.Subscribe(this);
+        }
+
+        public virtual void Unsubscribe()
+        {
+            unsubscriber.Dispose();
+        }
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(Observation value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
