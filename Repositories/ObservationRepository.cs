0b86739bc940d6af99de453fe6886bee76adcfd2
ywon0008 <ywon0008@student.monash.edu>
2019-05-17 22:34:24 +1000

    Repository updated to get tobacco and blood pressure

--- Repositories/ObservationRepository.cs
+++ Repositories/ObservationRepository.cs
@@ -15,6 +15,8 @@ namespace project.Repositories
         private readonly ObservationQuery ObservationQuery = new ObservationQuery();
         private readonly string ArgumentStart = "?";
         private readonly string TotalCholesterolArg = "code=http://loinc.org|2093-3";
+        private readonly string BloodPressureArg = "code=http://loinc.org|72166-2";
+        private readonly string TobaccoArg = "code=http://loinc.org|55284-4";
         private readonly string PatientArg = "patient=Patient/";
 
         /// <summary>
@@ -25,7 +27,20 @@ namespace project.Repositories
         public async Task<IEnumerable<Observation>> GetByPatientAndTotalCholesterol(string patientId)
         {
             string argument = ArgumentStart + TotalCholesterolArg + "&" + PatientArg + patientId;
-            var observations = await ObservationQuery.GetObservationsAsync(argument);
+            var observations = await ObservationQuery.GetObservationsAsync(Observation.ObservationType.Cholesterol, argument);
+            return observations;
+        }
+
+        public async Task<IEnumerable<Observation>> GetByPatientAndTobacco(string patientId)
+        {
+            string argument = ArgumentStart + TobaccoArg + "&" + PatientArg + patientId;
+            var observations = await ObservationQuery.GetObservationsAsync(Observation.ObservationType.Tobacco, argument);
+            return observations;
+        }
+        public async Task<IEnumerable<Observation>> GetByPatientAndBloodPressure(string patientId)
+        {
+            string argument = ArgumentStart + BloodPressureArg + "&" + PatientArg + patientId;
+            var observations = await ObservationQuery.GetObservationsAsync(Observation.ObservationType.BloodPressure, argument);
             return observations;
         }
     }
