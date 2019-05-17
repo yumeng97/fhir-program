8bbf3c7a2c06410d70f24c185074d79d1af8aa95
ywon0008 <ywon0008@student.monash.edu>
2019-05-17 22:32:36 +1000

    Observation to support different type such as cholesterol, blood pressure and tobacco

--- Models/Observation.cs
+++ Models/Observation.cs
@@ -7,8 +7,22 @@ namespace project.Models
 {
     public class Observation
     {
+        public enum ObservationType
+        {
+            Cholesterol, BloodPressure, Tobacco, DiastolicBloodPressure, SystolicBloodPressure
+        }
+
         public string PatientId { get; set; }
-        public string TotalCholesterol { get; set; } 
+        public string Value { get; set; }
+        public ObservationType Type { get; set; }
         public DateTime EffectiveDateTime { get; set; }
+
+        public Observation(string patientId, string value, ObservationType type, DateTime dateTime)
+        {
+            this.PatientId = patientId;
+            this.Value = value;
+            this.EffectiveDateTime = dateTime;
+            this.Type = type;
+        }
     }
 }
